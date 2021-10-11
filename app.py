0a74e3945f6f2b4e239c632e3346905da129334c
duypham228 <duy.cs.pham@gmail.com>
2021-10-11 17:16:43 -0500

    structuring and simple routes

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/', methods=['GET'])
-def hello_world():
-    return {
-        'hello': 'world'
-    }
+from flask import Flask, render_template, url_for
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/main_vis', methods=['GET'])
+def main_vis():
+    return render_template('main_vis.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
