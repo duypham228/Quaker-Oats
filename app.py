9a0a461525d2732eb290e88cfe36ad38bf8c72a9
duy-tamu <duy@tamu.edu>
2021-10-20 04:24:55 -0500

    integrate map in main

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -9,6 +9,10 @@ def home():
 @app.route('/main_vis', methods=['GET'])
 def main_vis():
     return render_template('main_vis.html')
+
+@app.route('/get_map_data')
+def get_map_data():
+    return "hello"
 
 
 if __name__ == "__main__":
