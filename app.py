28020172b1f44464f714cbd9b07a1adad032c1cf
hnathehe2 <hoangnghiaanh04@tamu.edu>
2021-10-28 18:09:58 -0500

    update on query

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
+
+
+# from models import Earthquake
+import models
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import text
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -8,6 +16,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:369491Nghia@localhost:5432/quaker_oats'
 db = SQLAlchemy(app)
 
+engine = create_engine('postgresql://postgres:369491Nghia@localhost:5432/quaker_oats')
+
+# engine = db.create_engine('postgresql+psycopg2://postgres:369491Nghia@localhost:5432/postgresql+psycopg2://postgres:369491Nghia@localhost:5432/quaker_oats')
+# connection = engine.connect()
 
 
 @app.route('/', methods=['GET'])
@@ -20,7 +32,19 @@ def main_vis():
 
 @app.route('/get_map_data')
 def get_map_data():
-    return "hello"
+    stmt = select(models.Earthquake) #.where(models.Earthquake.Vol != -1000)
+    session = Session(engine)
+    # result = session.execute(stmt).all()
+    result = session.query(models.Earthquake).all()
+    # result = session.execute(text("select * from earthquakes limit 10;"))
+    # print(type(stmt), '----------------------------')
+    # print(type(result[0]), '----------------------------------')
+    
+    # for row in result:
+        # print(row.__dict__)
+    
+    # return str(result)
+    return str(result[0].__dict__)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -88,7 +88,9 @@ class Earthquake(db.Model):
         self.Total_Houses_Damaged_Description = Total_Houses_Damaged_Description
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        # return 'ID {}'.format(self.ID)
+        return self
+
     
     def serialize(self):
         return {
@@ -134,26 +136,25 @@ class Earthquake(db.Model):
         }
 db.create_all()
 
-tsv_file = open("static/earthquake_data.tsv")
-read_tsv = csv.reader(tsv_file, delimiter="\t")
-count = 0
-for row in read_tsv:
-    if count > 0:
-        for i in range (4,39):
-            if row[i]=='':
-                row[i]='-1000'
-        new_data = Earthquake(  row[0],  row[1],  row[2],  row[3],  row[4],
-                                row[5],  row[6],  row[7],  row[8],  row[9],
-                                row[10], row[11], row[12], row[13], row[14],
-                                row[15], row[16], row[17], row[18], row[19],
-                                row[20], row[21], row[22], row[23], row[24],
-                                row[25], row[26], row[27], row[28], row[29],
-                                row[30], row[31], row[32], row[33], row[34], 
-                                row[35], row[36], row[37], row[38]
-        )
-        db.session.add(new_data)
-        db.session.commit()
-    count +=1
-# db.session.commit()
+# tsv_file = open("static/earthquake_data.tsv")
+# read_tsv = csv.reader(tsv_file, delimiter="\t")
+# count = 0
+# for row in read_tsv:
+#     if count > 0:
+#         for i in range (4,39):
+#             if row[i]=='':
+#                 row[i]='-1000'
+#         new_data = Earthquake(  row[0],  row[1],  row[2],  row[3],  row[4],
+#                                 row[5],  row[6],  row[7],  row[8],  row[9],
+#                                 row[10], row[11], row[12], row[13], row[14],
+#                                 row[15], row[16], row[17], row[18], row[19],
+#                                 row[20], row[21], row[22], row[23], row[24],
+#                                 row[25], row[26], row[27], row[28], row[29],
+#                                 row[30], row[31], row[32], row[33], row[34], 
+#                                 row[35], row[36], row[37], row[38]
+#         )
+#         db.session.add(new_data)
+#         db.session.commit()
+#     count +=1
   
 
